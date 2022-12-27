c885c006da8bf9e70a2664a1b10feea8508bf427
sonia-candia <109828763+sonia-candia@users.noreply.github.com>
2022-12-27 20:03:13 -0300

    Add files via upload

---
+++ tipos_datos.js
@@ -0,0 +1,10 @@
+let nombre= "Sonia";
+let edad= 26;
+let desarrollador= true;
+let fechaNacimiento= new Date(1996,11,17)
+const libroFavorito={
+    titulo: "Orgullo y prejuicio",
+    autor: "Jane Austen",
+    fecha: new Date(1813,0,28),
+    url: new URL("https://www.amazon.com/-/es/Orgullo-y-prejuicio/dp/6070732723") ,
+}
